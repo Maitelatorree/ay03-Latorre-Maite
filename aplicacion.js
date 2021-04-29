be767be5a7bcf0f4f5664c9a4f023f6c8fd87af4
maitelatorree <maite.latorre@uc.cl>
2021-04-29 17:01:30 -0400

    ayu java

---
+++ aplicacion.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+    var boton = $("#boton");
+    var boton2 = $("#boton2");
+    var aparecer = $("#aparecer");
+    var cambiar = $("#colorcito");
+
+    boton.mouseenter(function(){
+        $(this).fadeTo("fast",0.5)
+        
+    });
+    boton.mouseleave(function(){
+        $(this).fadeTo("fast",1)
+    });
+
+    boton.click(function(){ 
+        $("#esto").hide()
+
+    });
+    aparecer.click(function(){ 
+        $("#esto").show()
+
+    });
+    
+    boton2.click(function(){ 
+        $("#titulo").text("HOLAHOLAAA")
+
+    });
+
+    cambiar.mouseenter(function(){ 
+        $("#colorcito").css("color","pink")
+
+    });
+});
